8e6851d4e67437465633a93b03244500f7c6618b
Simon Smiley-Andrews <simon.smiley-andrews@suncorp.com.au>
2018-03-01 14:36:42 +1100

    first commit

---
+++ NomApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NomApp
+//
+//  Created by SMILEY-ANDREWS, Simon on 1/3/18.
+//  Copyright © 2018 SMILEY-ANDREWS, Simon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
